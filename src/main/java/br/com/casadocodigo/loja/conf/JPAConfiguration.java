b3c4713cd88bb45fbf299a7bf1890947ba1f2e94
schiozer <schiozer@yahoo.com.br>
2017-03-16 19:46:44 -0300

    deploy

--- src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -56,10 +56,9 @@ public class JPAConfiguration {
 		
 		Properties properties = new Properties();
 		
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		properties.setProperty("hibernate.show_sql", "true");
-		properties.setProperty("hibernate.hbm2ddl.auto", "create");
 		
 		return properties;
 	}
