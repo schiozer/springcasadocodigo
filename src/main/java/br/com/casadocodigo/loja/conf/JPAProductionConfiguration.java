2098e8174b34560909819b81f1d521603ff911a4
schiozer <schiozer@yahoo.com.br>
2017-03-15 21:47:33 -0300

    teste

--- src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
@@ -7,11 +7,12 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Profile("prod")
+@Configuration
 public class JPAProductionConfiguration {
 	
 	/*Podemos acessar qualquer variável de ambiente através da classe Environment, já disponibilizada pelo próprio Spring.*/
@@ -19,6 +20,7 @@ public class JPAProductionConfiguration {
 	private Environment environment;
 	
 	@Bean
+	@Profile("prod")
 	public DataSource dataSource() throws URISyntaxException{
 		
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
